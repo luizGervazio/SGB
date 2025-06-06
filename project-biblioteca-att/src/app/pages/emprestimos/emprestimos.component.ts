a20025e3d13006f33eb8b969f8b1787c1a077893
Luiz Gervazio <luizfdvgervazio@gmail.com>
2025-06-06 15:43:14 -0300

    fix

--- project-biblioteca-att/src/app/pages/emprestimos/emprestimos.component.ts
+++ project-biblioteca-att/src/app/pages/emprestimos/emprestimos.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FixedHeaderComponent } from '../../components/fixed-header/fixed-header.component';
-import { MainTableComponent } from "../../components/main-table/main-table.component";
+import { FixedHeaderComponentEmprestimo } from '../../components/fixed-header-emprestimo/fixed-header-emprestimo.component';
 import { RouterOutlet } from '@angular/router';
+import { MainTableEmprestimoComponent } from '../../components/main-table-emprestimo/main-table-emprestimo.component';
 
 @Component({
-  selector: 'app-emprestimos',
-  imports: [RouterOutlet,SidebarComponent, FixedHeaderComponent, HttpClientModule, MainTableComponent],
+  selector: 'app-emprestimo',
+  imports: [RouterOutlet, SidebarComponent, FixedHeaderComponentEmprestimo, MainTableEmprestimoComponent, HttpClientModule, ],
   templateUrl: './emprestimos.component.html',
 })
 export class EmprestimosComponent {
-  title = 'SGBE';
+    title = 'SGBE';
 
-  buscarLivro(value: string) {
+  buscarEmprestimo(value: string) {
     console.log('Busca:', value);
   }
 
-  abrirModalLivro() {
-    console.log('Abrir modal de livro');
+  abrirModalEmprestimo() {
+    console.log('Abrir modal de cliente');
   }
 }
