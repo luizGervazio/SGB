edfe23e5e4e70995e0c4ec063c37a49b9625c91c
Luiz Gervazio <luizfdvgervazio@gmail.com>
2025-06-06 15:43:14 -0300

    fix

--- project-biblioteca-att/src/app/pages/clientes/clientes.component.ts
+++ project-biblioteca-att/src/app/pages/clientes/clientes.component.ts
@@ -3,11 +3,11 @@ import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FixedHeaderComponentCliente } from '../../components/fixed-header-cliente/fixed-header-cliente.component';
 import { RouterOutlet } from '@angular/router';
-import { MainTableClienteComponent } from '../../components/main-table-cliente/main-table-cliente.component';
+import { MainTableComponent } from '../../components/main-table-cliente/main-table-cliente.component';
 
 @Component({
   selector: 'app-clientes',
-  imports: [RouterOutlet, SidebarComponent, FixedHeaderComponentCliente,MainTableClienteComponent ,HttpClientModule, ],
+  imports: [RouterOutlet, SidebarComponent, FixedHeaderComponentCliente, MainTableComponent, HttpClientModule, ],
   templateUrl: './clientes.component.html',
 })
 export class ClientesComponent {
