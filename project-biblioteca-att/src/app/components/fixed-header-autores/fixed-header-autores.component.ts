c4dcd60f9f1c71cdece17dc050be557fd60699d6
Luiz Gervazio <luizfdvgervazio@gmail.com>
2025-06-23 13:50:42 -0300

    att

--- project-biblioteca-att/src/app/components/fixed-header-autores/fixed-header-autores.component.ts
+++ project-biblioteca-att/src/app/components/fixed-header-autores/fixed-header-autores.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SidebarService } from '../../services/sidebar.service';
+import { AddAutorModalComponent } from '../../modals/add-autor-modal/add-autor-modal.component';
 
 @Component({
   selector: 'app-fixed-header-autores',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, AddAutorModalComponent], 
   templateUrl: './fixed-header-autores.component.html',
-  styleUrl: './fixed-header-autores.component.css'
+  styleUrls: ['./fixed-header-autores.component.css']
 })
-export class FixedHeaderAutoresComponent {
+export class FixedHeaderAutorComponent implements OnInit {
+  @Input() title: string = 'Título';
+  @Input() placeholder: string = 'Buscar...';
+  @Input() addButtonLabel: string = 'Adicionar';
+
+  @Output() onSearch = new EventEmitter<string>();
+  @Output() onAdd = new EventEmitter<any>(); // você pode remover isso se controlar tudo internamente
+
+  searchTerm: string = '';
+  isSidebarClosed = false;
+
+  showAddBookModal = false;
+
+  constructor(private sidebarService: SidebarService) {}
+
+  ngOnInit(): void {
+    this.sidebarService.sidebarState$.subscribe(state => {
+      this.isSidebarClosed = state;
+    });
+  }
+
+  openModal() {
+  this.showAddBookModal = true;
+}
+
+closeModal() {
+  this.showAddBookModal = false;
+}
+
+handleAddBook(book: any) {
+  console.log('Autor adicionado:', book);
+  this.closeModal();
+}
 
 }
--- project-biblioteca-att/src/app/pages/autores/autores.component.ts
+++ project-biblioteca-att/src/app/pages/autores/autores.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FixedHeaderComponentCliente } from '../../components/fixed-header-cliente/fixed-header-cliente.component';
+import { FixedHeaderAutorComponent } from '../../components/fixed-header-autores/fixed-header-autores.component';
 import { RouterOutlet } from '@angular/router';
 import { MainTableComponent } from '../../components/main-table-cliente/main-table-cliente.component';
 
 @Component({
   selector: 'app-autores',
-  imports: [RouterOutlet, SidebarComponent, FixedHeaderComponentCliente, MainTableComponent, HttpClientModule, ],
+  imports: [RouterOutlet, SidebarComponent, FixedHeaderAutorComponent, MainTableComponent, HttpClientModule, ],
   templateUrl: './autores.component.html',
 })
 export class AutoresComponent {
     title = 'SGBE';
 
-  buscarCliente(value: string) {
+  buscarAutor(value: string) {
     console.log('Busca:', value);
   }
 
-  abrirModalCliente() {
-    console.log('Abrir modal de cliente');
+  abrirModalAutor() {
+    console.log('Abrir modal de Autores');
   }
 }
